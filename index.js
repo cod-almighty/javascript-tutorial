1cca0020d6ffae8788b6f0cf164911b124217af7
Donnie Carroll <dcarroll29@dxc.com>
2021-07-30 13:00:38 +0100

    generating fretboard as table

--- index.js
+++ index.js
@@ -9,13 +9,41 @@ function nextNote(currentNote) {
     else noteIndex++;
     currentNote = chromaticScale[noteIndex]
 
-    return currentNote
+    return currentNote;
 }
 
-let note = "G";
+function generateTable(){
+    let body = document.getElementsByTagName("body")[0];
+    let tbl = document.createElement("table");
+    let tblBody = document.createElement("tbody");
+    let strings = ['E','B','G','D','A','E'];
 
-for (i = 0; i < 17; i++) {
+    for (let index in strings) {
+        let note = strings[index];
+
+        console.log(note);
+        let row = document.createElement("tr")
+
+        for (let j = 0; j < 18; j++) {
+            let cell = document.createElement("td");
+            let cellText = document.createTextNode(note);
+            cell.appendChild(cellText);
+            row.appendChild(cell);
+            note = nextNote(note);
+        }
+        tblBody.appendChild(row);
+    }
+
+    tbl.appendChild(tblBody);
+    body.appendChild(tbl);
+    tbl.setAttribute("border", "2");
+}
+
+
+generateTable();
+
+/* for (i = 0; i < 18; i++) {
     
     console.log("note" + i + ":" + note);
     note = nextNote(note);
-}
+} */
