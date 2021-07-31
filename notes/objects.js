339e11a7748fcce506e72690da8a15e1b63e306f
cod-almighty <donnie.carroll@gmail.com>
2021-07-31 16:51:24 +0100

    Object Working!

--- notes/objects.js
+++ notes/objects.js
@@ -1,46 +1,15 @@
-//object literal. Would need to copy for every new object. bugs can be multiplied doing this
-/* const circle = {
-    radius: 1,
-    location: {
-        x: 1,
-        y: 1
-    },
-    draw: function() { //object method aka behaviour
-        console.log('draw');
-    }
-};
+// Building objects from rootNote
 
-circle.draw(); */
-
-//Factory function, creates new objects from template
-function createCircle(radius){
-    return{
-        radius: radius,
-        draw: function() {
-            console.log('draw')
-        }
-    };
-}
-
-const circle = createCircle(1);
-circle.draw();
-
-//Constructor Function
-function Circle(radius) { //convention is PascalCase for constructor functions
-    this.radius = radius; //'this' refers to the globla object, Circle() in this case
-    this.draw = function() {
-        console.log('draw');
-    }
-}
-
-const another = new Circle(12);
-console.log(another.radius)
-
+const chromaticScale = ['C','C#','D','D#','E','F','F#','G','G#','A','A#','B'];
+const majorSteps = [2,2,1,2,2,2,1];
+const minorSteps = [2,1,2,2,1,2,2];
+const majorPentatonicSteps = [2,2,3,4];
+const minorPentatonicSteps = [2,1,4,3];
 
 //helper function to get next note in sequence
 function nextNote(currentNote, step = 1) {
-    //console.log(chromaticScale.length);
     let noteIndex = chromaticScale.indexOf(currentNote);
+
     if (noteIndex + step > chromaticScale.length - 1){
         noteIndex = (noteIndex + step) - chromaticScale.length;
     }
@@ -50,17 +19,29 @@ function nextNote(currentNote, step = 1) {
     return newNote;
 }
 
-const majorSteps = [2,2,1,2,2,2,1]
-const minorSteps = [2,1,2,2,1,2,2]
 function Scale(rootNote) {
+
+    let getScale = function(steps){
+        let note = rootNote;
+        let scale = [note];
+        steps.forEach(element => {
+            note = nextNote(note, element);
+            scale.push(note);
+        });
+        return scale;
+    };
+
     this.root = rootNote;
-    this.getMajor = [];
-    this.minor = [];
-    this.minorPentatonic = [];
-    this.majorPentatonic = [];
-    }
+    this.major = getScale(majorSteps);
+    this.minor = getScale(minorSteps);
+    this.majorPentatonic = getScale(majorPentatonicSteps);
+    this.minorPentatonic = getScale(minorPentatonicSteps);
 }
 
-chord = new Scale("G");d
-chord.getScales();
-console.log(`chord is [${chord.minor}]`);
+
+chord = new Scale("A");
+
+console.log(`minor scale is [${chord.minor}]`);
+console.log(`major scale is [${chord.major}]`);
+console.log(`major pent is [${chord.majorPentatonic}]`);
+console.log(`minor pent is [${chord.minorPentatonic}]`);
